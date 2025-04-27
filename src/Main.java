ed7374245a06e3227722e34feda45413a5f2ea56
Diego <diego.1996pessoal@gmail.com>
2025-04-27 17:03:09 -0300

    Resolvendo desafios da lista tarefa e lista compra.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import list.ListaTarefa;
+import list.Tarefa;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ListaTarefa lista = new ListaTarefa();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+        lista.adicionarTarefa("teste1");
+        lista.adicionarTarefa("teste1");
+        lista.adicionarTarefa("teste3");
+        lista.adicionarTarefa("teste4");
+
+
+        System.out.println("Numero total de tarefas é igual a ..:"+ lista.obterNumeroTotalTarefas());
+
+        lista.removerTarefa("teste1");
+        System.out.println("Numero total de tarefas é igual a ..:"+ lista.obterNumeroTotalTarefas());
+        lista.obterDescricoesTarefaS();
+}
 }
---
+++ src/carrinhodecompras/CarrinhoDeCompras.java
@@ -0,0 +1,44 @@
+package carrinhodecompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    List<Item> itens;
+
+    public CarrinhoDeCompras() {
+        this.itens = itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item i = new Item(nome, preco, quantidade);
+        this.itens.add(i);
+    }
+
+    public void exibirItems(){
+        System.out.println(itens);
+    }
+
+    public String calcularValorTotal(){
+        float valorTotal = 0f;
+        for (Item i : itens){
+            float valor = (float) (i.getPreco() * i.getQuantidade());
+            valorTotal = valorTotal + valor;
+
+        }
+        return String.format("Valor Total...: R$:%.2f", valorTotal);
+    }
+
+    public void removerItem(String nome){
+        List<Item> removerItem = new ArrayList<>();
+
+        for(Item i : itens){
+            if (i.getNome().equalsIgnoreCase(nome)){
+                removerItem.add(i);
+            }
+        }
+        itens.removeAll(removerItem);
+    }
+    
+}
